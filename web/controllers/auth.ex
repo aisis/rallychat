ff22f8a4792ee42cbcb3e06800cc5b2ca778addb
Aisis <aisiszane@gmail.com>
2017-03-27 19:43:00 -1000

    still working on it

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -1,5 +1,6 @@
 defmodule Rallychat.Auth do
     import Plug.Conn
+    import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
     
     def init(opts) do
         Keyword.fetch!(opts, :repo)
@@ -7,7 +8,47 @@ defmodule Rallychat.Auth do
 
     def call(conn, repo) do
         user_id = get_session(conn, :user_id)
-        user    = user_id && repo.get(Rallychat.User, user_id)
+        user = user_id && repo.get(Rallychat.User, user_id)
         assign(conn, :current_user, user)
+    end
+
+    def login(conn, user) do
+        conn
+        |> assign(:current_user, user.id)
+        |> put_session(:user_id, user.id)
+        |> configure_session(renew: true)
+    end
+    
+    def login_by_username_pass(conn, username, given_pass, opts) do
+        repo = Keyword.fetch!(opts, :repo)
+        user = repo.get_by(Rallychat.User, username: username)
+
+        cond do
+            user && checkpw(give_pass, user.password_hash) ->
+                {:ok, login(conn, user)}
+            user ->
+                {:error, :unauthorized, conn}
+            true ->
+                dummy_checkpw()
+                {:error, :not_found, conn}
+        end
+    end
+
+    def logout(conn) do
+        configure_session(conn, drop: true)
+    end
+
+    import Phoenix.Controller
+    alias Rallychat.Router.Helpers
+
+    def authenticate_user(conn, _opts) do
+        if conn.assigns.current_user do 
+            conn
+        else
+            conn
+            |> put_flash(:error, "you must be logged in for access")
+            |> redirect(to: Helpers.page_path(conn, :index))
+            |> halt()
+        end
     end
 end
--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -4,4 +4,19 @@ defmodule Rallychat.SessionController do
     def new(conn, _) do
         render conn, "new.html"
     end
+
+    def create(conn, %{"session" => %{"username" => user, "password" => pass}}) do
+        case Rallychat.Auth.login_by_username_pass(conn, user, pass, repo, repo: Repo) do
+            {:ok, conn} ->
+                conn
+                |> put_flash(:info, "welcome back")
+                |> render("new.html")
+        end
+    end
+
+    def delete(conn, _) do
+        conn
+        |> Rallychat.Auth.logout()
+        |> redirect(to: page_path(conn, :index))
+    end
 end
